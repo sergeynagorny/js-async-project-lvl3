5b3dd113c6c8c427872858f055055fbc7ae9759d
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-12-12 14:13:08 +0300

    feat: load-files by tags

--- src/load-page-files.js
+++ src/load-page-files.js
@@ -29,9 +29,47 @@ export default function loadPageFiles({ html, requestUrl, outputPath }) {
             $(element).attr('src', fileRelativePath)
         })
 
+    $.root()
+        .find('link')
+        .each((i, element) => {
+            let elementSrc = $(element).attr('href')
+            const elementRel = $(element).attr('rel')
+
+            if (elementRel !== 'stylesheet') {
+                elementSrc += '.html'
+            }
+
+            if (new URL(requestUrl).origin !== new URL(elementSrc, requestUrl).origin) {
+                return
+            }
+
+            const { href: fileHref } = new URL(elementSrc, requestUrl)
+            const fileRelativePath = path.join(filesDirRelativePath, createName(fileHref))
+            const fileAbsolutePath = path.join(outputPath, fileRelativePath)
+            promises.push(getFilePromise(fileHref, fileAbsolutePath))
+            $(element).attr('href', fileRelativePath)
+        })
+
+    $.root()
+        .find('script')
+        .each((i, element) => {
+            const elementSrc = $(element).attr('src')
+
+            if (new URL(requestUrl).origin !== new URL(elementSrc, requestUrl).origin) {
+                return
+            }
+
+            const { href: fileHref } = new URL(elementSrc, requestUrl)
+            const fileRelativePath = path.join(filesDirRelativePath, createName(fileHref))
+            const fileAbsolutePath = path.join(outputPath, fileRelativePath)
+            promises.push(getFilePromise(fileHref, fileAbsolutePath))
+            $(element).attr('src', fileRelativePath)
+        })
+
     return fs
         .mkdir(filesDirAbsolutePath)
         .catch(_.noop) // suppress an error if the directory has already been created
         .then(() => Promise.all(promises))
+        .catch(_.noop) // suppress an error if the file has 404 status
         .then(() => ({ html: $.html() }))
 }
